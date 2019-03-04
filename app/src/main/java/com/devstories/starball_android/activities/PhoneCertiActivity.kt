3815c76ab419946cf81a10edc57f91646e5c4ce8
youngju <youngjucha0316@gmail.com>
2019-03-04 12:26:42 +0900

    동우등록

--- app/src/main/java/com/devstories/starball_android/activities/PhoneCertiActivity.kt
+++ app/src/main/java/com/devstories/starball_android/activities/PhoneCertiActivity.kt
@@ -4,9 +4,21 @@ import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.devstories.starball_android.Actions.MemberAction
 import com.devstories.starball_android.R
+import com.devstories.starball_android.base.PrefUtils
 import com.devstories.starball_android.base.RootActivity
+import com.devstories.starball_android.base.Utils
+import com.loopj.android.http.JsonHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
 import kotlinx.android.synthetic.main.activity_phone_certi.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class PhoneCertiActivity : RootActivity() {
 
@@ -29,9 +41,130 @@ class PhoneCertiActivity : RootActivity() {
         backIV.setOnClickListener {
             finish()
         }
+        phoneET.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
 
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.length ==11){
+                    okTV.text = getString(R.string.phone_certi)
+                }else{
+                    okTV.text = getString(R.string.phone_confirm_title)
+                }
+
+            }
+        })
     }
 
+
+
+    fun phoneconfirm() {
+        val member_id = PrefUtils.getIntPreference(context, "member_id")
+        val phone:String = Utils.getString(phoneET)
+
+        if (phoneET.length()< 11){
+            Toast.makeText(this,"핸드폰번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        val params = RequestParams()
+        params.put("member_id", member_id)
+        params.put("phone", phone)
+
+        MemberAction.login(params, object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                try {
+                    val result = response!!.getString("result")
+
+                    if ("ok" == result) {
+                        Toast.makeText(context, "인증 번호가 발송하였습니다.", Toast.LENGTH_LONG).show()
+                        val intent = Intent(context, PhoneCerti2Activity::class.java)
+                        startActivity(intent)
+                    } else {
+
+                        val message = response!!.getString("message")
+
+                        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                    }
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONArray?) {
+                super.onSuccess(statusCode, headers, response)
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, responseString: String?) {
+
+                // System.out.println(responseString);
+            }
+
+            private fun error() {
+                Utils.alert(context, "조회중 장애가 발생하였습니다.")
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, responseString: String?, throwable: Throwable) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                System.out.println(responseString);
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, throwable: Throwable, errorResponse: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                System.out.println(errorResponse);
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, throwable: Throwable, errorResponse: JSONArray?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                System.out.println(errorResponse);
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onStart() {
+                // show dialog
+                if (progressDialog != null) {
+
+                    progressDialog!!.show()
+                }
+            }
+
+            override fun onFinish() {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+            }
+        })
+    }
 
 
 }
