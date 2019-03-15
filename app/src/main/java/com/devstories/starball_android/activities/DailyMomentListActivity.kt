03b50e65451e7ce79ce12deb8dd171436c83a30e
youngju <youngjucha0316@gmail.com>
2019-03-15 11:36:30 +0900

    동우등록

--- app/src/main/java/com/devstories/starball_android/activities/DailyMomentListActivity.kt
+++ app/src/main/java/com/devstories/starball_android/activities/DailyMomentListActivity.kt
@@ -1,9 +1,16 @@
 package com.devstories.starball_android.activities
 
+import android.Manifest
 import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -23,6 +30,9 @@ class DailyMomentListActivity : RootActivity() {
 
     lateinit var DaillyAdapter: DaillyAdapter
 
+    private val FROM_ALBUM = 101
+    private val REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 2
+    private var selectedImage: Bitmap? = null
 
     lateinit var header: View
     lateinit var backIV: ImageView
@@ -60,12 +70,14 @@ class DailyMomentListActivity : RootActivity() {
 
         }
         videoLL.setOnClickListener {
-            val intent = Intent(context, DailyMomentViewListActivity::class.java)
-            startActivity(intent)
+
         }
         photoLL.setOnClickListener {
-            val intent = Intent(context, DailyMomentSubVIewListActivity::class.java)
-            startActivity(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                loadPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE)
+            } else {
+                imageFromGallery()
+            }
         }
 
 
@@ -77,5 +89,69 @@ class DailyMomentListActivity : RootActivity() {
 
     }
 
+    private fun loadPermissions(perm: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this, perm) !== PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(perm), requestCode)
+        } else {
+            imageFromGallery()
+        }
+    }
 
+    private fun imageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, FROM_ALBUM)
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_PERMISSION_READ_EXTERNAL_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    imageFromGallery()
+                } else {
+                    // no granted
+                }
+                return
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                FROM_ALBUM -> {
+                    if (data != null && data.data != null) {
+                        val selectedImageUri = data.data
+
+                        val filePathColumn = arrayOf(MediaStore.MediaColumns.DATA)
+
+                        val cursor =
+                            context!!.contentResolver.query(selectedImageUri!!, filePathColumn, null, null, null)
+                        if (cursor!!.moveToFirst()) {
+                            val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+                            val picturePath = cursor.getString(columnIndex)
+                            cursor.close()
+
+                            selectedImage = Utils.getImage(context!!.contentResolver, picturePath)
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (progressDialog != null) {
+            progressDialog!!.dismiss()
+        }
+
+    }
 }
