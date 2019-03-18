4927b9e4648391ce5f73720f7436ab36669d4d1a
youngju <youngjucha0316@gmail.com>
2019-03-18 19:38:21 +0900

    동우등록

--- app/src/main/java/com/devstories/starball_android/actions/MemberAction.kt
+++ app/src/main/java/com/devstories/starball_android/actions/MemberAction.kt
@@ -13,36 +13,47 @@ object MemberAction {
     fun match_list(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/match_list", params, handler)
     }
+
     fun login(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/login", params, handler)
     }
+
     fun get_info(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/info", params, handler)
     }
+
     fun profile_del(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/profile_del", params, handler)
     }
+
     fun list(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/list", params, handler)
     }
+
     fun edit(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/edit", params, handler)
     }
+
     fun secession(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/secession", params, handler)
     }
+
     fun starball_history(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/starball_history", params, handler)
     }
+
     fun cash_request(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/cash_request", params, handler)
     }
+
     fun buy_starball(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/buy_starball", params, handler)
     }
+
     fun free_starball(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/free_starball", params, handler)
     }
+
     fun like(params: RequestParams, handler: JsonHttpResponseHandler) {
         HttpClient.post("/members/like", params, handler)
     }
