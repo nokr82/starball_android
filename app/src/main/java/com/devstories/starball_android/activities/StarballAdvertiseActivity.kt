55be2aa8a89906c40e9d8133d0ac8799a24861d8
youngju <youngjucha0316@gmail.com>
2019-03-19 22:01:38 +0900

    동우등록

--- app/src/main/java/com/devstories/starball_android/activities/StarballAdvertiseActivity.kt
+++ app/src/main/java/com/devstories/starball_android/activities/StarballAdvertiseActivity.kt
@@ -3,12 +3,18 @@ package com.devstories.starball_android.activities
 import android.os.Bundle
 import com.devstories.starball_android.R
 import com.devstories.starball_android.base.RootActivity
+import kotlinx.android.synthetic.main.activity_starball_advertise.*
 
 class StarballAdvertiseActivity : RootActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starball_advertise)
+
+
+        closeIV.setOnClickListener {
+            finish()
+        }
 
     }
 
