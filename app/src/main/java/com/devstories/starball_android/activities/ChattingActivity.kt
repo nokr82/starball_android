5d2b3f8af92093d686f88fa627211220197eb633
MinJeung <zarg43@devstories.com>
2019-03-21 12:53:26 +0900

    민정 등록

--- app/src/main/java/com/devstories/starball_android/activities/ChattingActivity.kt
+++ app/src/main/java/com/devstories/starball_android/activities/ChattingActivity.kt
@@ -1,13 +1,24 @@
 package com.devstories.starball_android.activities
 
 import android.app.ProgressDialog
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.view.View
 import com.devstories.starball_android.R
+import com.devstories.starball_android.actions.MemberAction
+import com.devstories.starball_android.base.PrefUtils
+import com.devstories.starball_android.base.Utils
+import com.loopj.android.http.JsonHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
 import kotlinx.android.synthetic.main.activity_chatting.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class ChattingActivity : FragmentActivity() {
@@ -21,17 +32,135 @@ class ChattingActivity : FragmentActivity() {
     val ChattingCrushFragment: ChattingCrushFragment = ChattingCrushFragment()
     val ChattingSendCrushFragment: ChattingSendCrushFragment = ChattingSendCrushFragment()
 
+    var member_id = -1
+
+    internal var loungeCountReceiver: BroadcastReceiver? = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+            if (intent != null) {
+                loadData()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting)
+
+        var filter1 = IntentFilter("LOUNGE_COUNT")
+        registerReceiver(loungeCountReceiver, filter1)
+
         this.context = this
         progressDialog = ProgressDialog(context, com.devstories.starball_android.R.style.CustomProgressBar)
         progressDialog!!.setProgressStyle(android.R.style.Widget_DeviceDefault_Light_ProgressBar_Large)
 
+        member_id = PrefUtils.getIntPreference(context, "member_id")
+
         supportFragmentManager.beginTransaction().replace(R.id.chattingFL, ChattingFragment).commit()
 
         click()
+
+        loadData()
+    }
+
+    fun loadData() {
+
+        val params = RequestParams()
+        params.put("member_id", member_id)
+
+        MemberAction.lounge_count(params, object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                try {
+                    val result = response!!.getString("result")
+
+                    if ("ok" == result) {
+
+                        val send_like_read = Utils.getInt(response, "send_like_read")
+                        val receive_like_read = Utils.getInt(response, "receive_like_read")
+                        val match_read = Utils.getInt(response, "match_read")
+                        val chatting_read = Utils.getInt(response, "chatting_read")
+
+                        if (send_like_read > 0) {
+                            newSendIV.visibility = View.VISIBLE
+                        }
+
+                        if (receive_like_read > 0) {
+                            newReceiveIV.visibility = View.VISIBLE
+                        }
+
+                        if (match_read > 0) {
+                            newMatchIV.visibility = View.VISIBLE
+                        }
+
+                        if (chatting_read > 0) {
+                            newChattingIV.visibility = View.VISIBLE
+                        }
+
+                    } else {
+
+                    }
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, responseString: String?) {
+
+                System.out.println(responseString);
+            }
+
+            private fun error() {
+                Utils.alert(context, getString(R.string.api_error))
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, responseString: String?, throwable: Throwable) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+//                 System.out.println(responseString);
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, throwable: Throwable, errorResponse: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, throwable: Throwable, errorResponse: JSONArray?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onStart() {
+                // show dialog
+                if (progressDialog != null) {
+                    progressDialog!!.show()
+                }
+            }
+
+            override fun onFinish() {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+            }
+        })
     }
 
     fun click() {
@@ -133,5 +262,19 @@ class ChattingActivity : FragmentActivity() {
 
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        try {
+
+            if (loungeCountReceiver != null) {
+                unregisterReceiver(loungeCountReceiver)
+            }
+
+        } catch (e: IllegalArgumentException) {
+
+        }
+
+    }
 
 }
